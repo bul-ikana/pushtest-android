ef821f3fe69d277e2b7260057c9448437c7b36ec
Hugo Aguirre <bul.ikana@gmail.com>
2017-08-07 16:28:21 -0500

    Empty android project

---
+++ app/src/main/java/pushtest/mecabotware/com/pushtest/MainActivity.java
@@ -0,0 +1,13 @@
+package pushtest.mecabotware.com.pushtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {z
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
